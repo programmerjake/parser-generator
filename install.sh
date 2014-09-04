d8111954343bcf1bd3034375be59abf50b2fe86c
programmerjake <programmerjake@gmail.com>
2014-09-03 23:21:20 -0700

    changed install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-install_file=bin/Release/parser-generator
+install_file=parser-generator
 dest_dir=/usr/local/bin/
 
 if [ ! -x "$install_file" ]; then
     echo "can't find $install_file" >&2
     exit 1
 elif [ "a`whoami`" != "aroot" ]; then
-    echo "running sudo to change to root"
-    sudo -- "$0" "$@"
-    exit
+    echo "must be run as root" >&2
+    exit 1
 fi
 echo "installing $install_file to $dest_dir"
 install "$install_file" "$dest_dir"
